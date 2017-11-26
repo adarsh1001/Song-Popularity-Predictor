72451f8ffb10e2932309b979700bd43be3623675
AakashKT <aku2162@gmail.com>
2017-11-26 17:07:24 +0530

    Indentation fix

--- src/save_features.py
+++ src/save_features.py
@@ -6,37 +6,35 @@ song_dir = 'temp';
 
 def get_playcount_bin(playcount):
 
-	if playcount >= 1000000:
+	if playcount >= 5000000:
 		return 1;
-	else if playcount < 1000000 and playcount >= 500000:
+	elif playcount < 5000000 and playcount >= 1000000:
 		return 2;
-	else if playcount < 500000 and playcount >= 100000:
+	elif playcount < 1000000 and playcount >= 100000:
 		return 3;
-	else if playcount < 100000:
+	elif playcount < 100000:
 		return 4;
 
 
 def get_playcount(name):
-	l = os.listdir('../%s/' % song_dir);
 	f = json.load(open('../downloaded_songs.json'));
 	playcount = -1;
 
-	for item in l:
-		for key, value in f.iteritems():
-			temp = key.split(' ')[0].strip().lower();
-			temp2 = key.split(',')[0].strip().lower();
-			temp3 = key.split('.')[0].strip().lower();
+	for key, value in f.iteritems():
+	    temp = key.split(' ')[0].strip().lower();
+	    temp2 = key.split(',')[0].strip().lower();
+	    temp3 = key.split('.')[0].strip().lower();
 
-			if temp in item.lower() or temp2 in item.lower() or temp3 in item.lower():
-				for i in value:
-					t = i['song_name'].split(' ')[0].strip().lower();
-					t1 = i['song_name'].split(',')[0].strip().lower();
-					t2 = i['song_name'].split('.')[0].strip().lower();
+ 	    if temp in name.lower() or temp2 in name.lower() or temp3 in name.lower():
+	    for i in value:
+	        t = i['song_name'].split(' ')[0].strip().lower();
+		t1 = i['song_name'].split(',')[0].strip().lower();
+		t2 = i['song_name'].split('.')[0].strip().lower();
 
-					if t in item.lower() or t1 in item.lower() or t2 in item.lower():
-						playcount = int(i['playcount']);
-						break;
-				break;
+		if t in name.lower() or t1 in name.lower() or t2 in name.lower():
+		    playcount = int(i['playcount']);
+		    break;
+	    break;
 
 	return playcount;
 
@@ -51,10 +49,17 @@ if __name__ == '__main__':
 		f_new = [];
 		f_here = f[i*30:(i+1)*30];
 		for k in f_here:
-			f_new.append(sum(k)/len(l));
+			f_new.append(sum(k)/len(k));
 
 		playcount = get_playcount(w[i]);
-		f_new.append(get_playcount_bin(playcount));
+		print playcount
+                f_new.append(get_playcount_bin(playcount));
 
 		f_final.append(f_new);
+        print len(f_final);
+        print len(f_final[0]);
 
+        for item in f_final:
+            print item
+	    if item[-1] != 1:
+                print item;
